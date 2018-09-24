077a451abe5983665ff5e475559d35b52c9033f4
3RebeccaSila3 <silarebecca26@gmail.com>
2018-09-24 11:06:45 +0300

    Import http service nad inject it to the constructor

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../user'
+import { User } from '../user';
+import { Repository } from '../repository';
+import { HttpRequestService } from '../http-request.service';
 
 @Component({
   selector: 'app-search',
@@ -7,10 +9,12 @@ import { User } from '../user'
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  
+  user:any[];
+  repository:any[];
+  username:string;
 
 
-  constructor() { 
+  constructor(private httpRequest:HttpRequestService) { 
     
   }
 
