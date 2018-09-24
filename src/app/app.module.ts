a9a9e11015a8a45c3eca1ac554d120e114a1ee26
3RebeccaSila3 <silarebecca26@gmail.com>
2018-09-24 08:29:55 +0300

    Create component search to handle the logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SearchFormComponent } from './search-form/search-form.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchFormComponent
+    SearchFormComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
