1be4ed65fb995a18de10c45fceb43eb6440fcd68
3RebeccaSila3 <silarebecca26@gmail.com>
2018-09-24 08:21:20 +0300

    Create a form for entering github username

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule
